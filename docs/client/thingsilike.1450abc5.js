b2966231c0237fc6bceab99ab4ccccd54dc64312
zachary endrulat <zendrulat@gmail.com>
2020-06-04 21:25:13 -0500

    docs

---
+++ docs/client/thingsilike.1450abc5.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as i}from"./client.2d2660bd.js";const e="this thins from thinsilike";export default class extends s{constructor(s){super(),t(this,s,null,null,i,{})}}export{e as Elements};
